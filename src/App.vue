3c330218ec577a2395f882f52bce833c634a05da
Gregor Laan <laangregor@gmail.com>
2021-09-12 17:27:58 +0300

    Init textarea and fileUpload component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UploadForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UploadForm from './components/UploadForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UploadForm
   }
 }
 </script>
---
+++ src/components/UploadForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <FileUpload
+      name="image[]"
+      :multiple="false"
+      accept="image/*"
+      :max-file-size="1000000"
+      @upload="onUpload"
+    >
+      <template #empty>
+        <p>Drag and drop files to here to upload.</p>
+      </template>
+    </FileUpload>
+
+    <Textarea
+      v-model="caption"
+      :auto-resize="true"
+      placeholder="Caption"
+      rows="5"
+      cols="100"
+    />
+  </div>
+</template>
+
+<script>
+import Textarea from "primevue/textarea";
+import FileUpload from "primevue/fileupload";
+
+export default {
+  name: "UploadForm",
+
+  components: {
+    Textarea,
+    FileUpload,
+  },
+
+  data() {
+    return {
+      caption: "",
+    };
+  },
+
+  methods: {
+    onUpload: function () {},
+  },
+};
+</script>
+
+<style scoped>
+</style>
