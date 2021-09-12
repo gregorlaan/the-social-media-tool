e17f83a073ed2ac7954eae0a5911a7ca669659c1
Gregor Laan <laangregor@gmail.com>
2021-09-12 17:24:03 +0300

    Install primeicons and primevue saga theme

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/saga-blue/theme.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.mount('#app');
