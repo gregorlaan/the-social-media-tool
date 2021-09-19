c4b3425a6b52197c0dad30601eac2ac6625dff55
Gregor Laan <laangregor@gmail.com>
2021-09-19 12:33:05 +0300

    Replace PrimeVue form elements with TailwindCSS

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,42 +1,83 @@
 <template>
   <div>
-    <FileUpload
-      name="image[]"
-      :multiple="false"
-      accept="image/*"
-      :max-file-size="1000000"
-      @upload="onUpload"
-    >
-      <template #empty>
-        <p>Drag and drop files to here to upload.</p>
-      </template>
-    </FileUpload>
+    <div class="container bg-gray-100 p-4 rounded-lg">
+      <div class="row">
+        <div class="col-12 mb-4">
+          <label>Image</label>
 
-    <Textarea
-      v-model="caption"
-      :auto-resize="true"
-      placeholder="Caption"
-      rows="5"
-      cols="100"
-    />
+          <label
+            for="file"
+            class="block cursor-pointer text-center p-10 border border-dashed rounded-lg border-gray-300 bg-white"
+          >
+            Drag your files here or click in this area.
+          </label>
+
+          <input
+            id="file"
+            type="file"
+            name="file"
+            multiple
+            class="w-0 h-0 hidden"
+          >
+        </div>
+
+        <div class="col-12 mb-4">
+          <div class="flex flex-col space-y-2">
+            <label
+              for="default"
+              class="text-gray-700 select-none font-medium"
+            >
+              Title
+            </label>
+
+            <input
+              id="title"
+              v-model="title"
+              name="title"
+              type="text"
+              placeholder="Title"
+              class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            >
+          </div>
+        </div>
+
+        <div class="col-12 mb-4">
+          <div class="flex flex-col space-y-2">
+            <label
+              for="default"
+              class="text-gray-700 select-none font-medium"
+            >
+              Description
+            </label>
+
+            <input
+              id="description"
+              v-model="description"
+              name="description"
+              type="text"
+              placeholder="Description"
+              class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            >
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Textarea from "primevue/textarea";
-import FileUpload from "primevue/fileupload";
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 
 export default {
-  name: "UploadForm",
+  name: 'UploadForm',
 
   components: {
-    Textarea,
-    FileUpload,
   },
 
   data() {
     return {
-      caption: "",
+      description: '',
+      title: '',
     };
   },
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import PrimeVue from 'primevue/config';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
-import 'primevue/resources/themes/saga-blue/theme.css';
-import './assets/tailwind.css'
+import './assets/tailwind.css';
 
 const app = createApp(App);
 
-app.use(PrimeVue);
 app.mount('#app');
