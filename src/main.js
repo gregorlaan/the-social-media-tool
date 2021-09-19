189619bf599c3a94a7bff0c1d2a33addb701a13b
Gregor Laan <laangregor@gmail.com>
2021-09-19 11:15:33 +0300

    Install TailwindCSS using vue plugin

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css'
+import 'primeicons/primeicons.css';
 import 'primevue/resources/themes/saga-blue/theme.css';
+import './assets/tailwind.css'
 
 const app = createApp(App);
 
